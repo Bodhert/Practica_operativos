0b85d2c751bb1c32a3960c3d3e5d5bd0cff3d4ab
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-06-07 14:03:56 -0500

    adding the main

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "controlewe.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string arg1 = (argc > 1) ?  string(argv[1]) : "" ;
+    string arg2 = (argc > 2) ?  string(argv[2]) : "" ;
+    string arg3 = (argc > 3) ?  string(argv[3]) : "" ;
+
+    if(arg1 != "-n")
+    {
+        // cout << arg1 << endl;
+        cout << "usage -n <nommemcom>" << endl;
+        if(arg2 == "")
+        {
+            // cout << arg2 << "hola" << endl;
+            cout << "usage -n <nommemcom>" << endl;
+            return 1;
+        }
+        return 1;
+    }
+
+    cout << arg2 << endl;
+
+    controlewe *controler = new controlewe(arg2, "bews");
+    controler->readMemg(arg3);
+    //controler->readMemg()
+    return 0;
+}
