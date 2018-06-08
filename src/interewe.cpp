fe4af024617a6499892f41309a0f8631b6e1659b
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-06-08 02:45:13 -0500

    working in instruction

--- src/interewe.cpp
+++ src/interewe.cpp
@@ -11,13 +11,39 @@ void interewe::readBew(string bew)
 	cout << bew << endl;
 	ifstream fileToRead(bew.c_str(), ios::binary);
 
-	int hexNum;
-	while(fileToRead.read((char *)&hexNum, sizeof(char)))
-	{				
-		stringstream ss;
-        ss << hex << hexNum;
-        word_ = ss.str();
-		cout << hex << "word: " << word_ << endl;
+	// sometimes it fails we do not why
+	unsigned long long addr , opcode;
+	unsigned char hexNum;
+	while(fileToRead.read((char *)&hexNum, sizeof(unsigned char)))
+	{		
+		// cout << hex << hexNum << endl;
+		// int num = hexNum;
+		opcode = hexNum >> 4;
+		addr = (hexNum & 0xF);
+		
+		switch(opcode)
+		{
+			case 0:
+				for(int i = 0; i < 7; ++i)
+				{
+					fileToRead.read((char *)&hexNum, sizeof(unsigned char));
+					addr <<= 8;
+					addr |= hexNum;
+				}
+				addr >>= 30;
+				int memref = (addr >> 15);
+				int integer = (addr & 0x7FFF);
+
+				cout << "clean addr: " <<  hex <<addr << endl;
+				cout << " memref:" << hex << memref << " integer: " << integer << endl;
+				
+				break;
+		}
+
+		// stringstream ss;
+        // ss << hex << hexNum;
+        // word_ = ss.str();
+		// cout << hex << "word: " << hexNum << endl;
 	}
 }
 
@@ -25,8 +51,3 @@ interewe::~interewe()
 {
 
 }
-
-
-	
-
-
